f5726fbe5fcdd61acf73d7d5d59954bbfe208df8
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-01-12 21:35:40 -0800

    added function layouts for 2 of my about me questions to ask the user when they visit my site

--- js/app.js
+++ js/app.js
@@ -1,16 +1,27 @@
 'use strict';
 
-console.log('hey girl, hey!');
+// console.log('hey girl, hey!');
 
 // get username and give greeting
 var userName = prompt('Hi! What is your name?');
 alert('Hey ' + userName + ', glad you\'re here!');
 
-// write 5 questions. must accept yes or no OR y or n IN ANY CASE
+// write 5 questions. must accept yes or no OR y or n IN ANY CASE - final question use username
 
 var questionOne = prompt('Question One?').toLowerCase();
 
 if (questionOne === 'yes' || questionOne === 'y') {
   // console.log('you are right!');
   alert('you are right!');
+} else {
+  alert('womp womp...that\'s wrong');
+}
+
+var questionTwo = prompt('Question two?').toLowerCase();
+
+if (questionTwo === 'yes' || questionTwo === 'y') {
+  // console.log('you are right!');
+  alert('you are right!');
+} else {
+  alert('womp womp...that\'s wrong');
 }
