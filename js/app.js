3e78d70471589e0f84da9a4160fb76c5ada59411
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-01-13 18:53:54 -0800

    renamed my variable names to my prompt questions

--- js/app.js
+++ js/app.js
@@ -8,45 +8,45 @@ alert('Hey ' + userName + ', glad you\'re here!');
 
 // write 5 questions. must accept yes or no OR y or n IN ANY CASE - final question use username
 
-var questionOne = prompt('Am I originally from Seattle, WA?').toLowerCase();
+var responseOne = prompt('Am I originally from Seattle, WA?').toLowerCase();
 
-if (questionOne === 'no' || questionOne === 'n') {
+if (responseOne === 'no' || responseOne === 'n') {
   // console.log('You are right!');
   alert('You are right!');
 } else {
   alert('Womp womp...that\'s wrong');
 }
 
-var questionTwo = prompt('Do I have a daughter?').toLowerCase();
+var responseTwo = prompt('Do I have a daughter?').toLowerCase();
 
-if (questionTwo === 'yes' || questionTwo === 'y') {
+if (responseTwo === 'yes' || responseTwo === 'y') {
   // console.log('Correct!');
   alert('Correct!');
 } else {
   alert('Sorry, incorrect!');
 }
 
-var questionThree = prompt('Do I have an obession with celebrities?').toLowerCase();
+var responseThree = prompt('Do I have an obession with celebrities?').toLowerCase();
 
-if (questionThree === 'yes' || questionThree === 'y') {
+if (responseThree === 'yes' || responseThree === 'y') {
   // console.log('you betcha! no shame in it here');
   alert('You betcha! No shame in it here');
 } else {
   alert('Sorry, incorrect!');
 }
 
-var questionFour = prompt('Do I try to act younger and hipper than I really am?').toLowerCase();
+var responseFour = prompt('Do I try to act younger and hipper than I really am?').toLowerCase();
 
-if (questionFour === 'yes' || questionFour === 'y') {
+if (responseFour === 'yes' || responseFour === 'y') {
   // console.log('Yass Queen!');
   alert('Yass Queen!');
 } else {
   alert('Womp womp..that\'s wrong');
 }
 
-var questionFive = prompt('Do I have more tattoos than I can count on one hand?').toLowerCase();
+var responseFive = prompt('Do I have more tattoos than I can count on one hand?').toLowerCase();
 
-if (questionFive === 'yes' || questionFive === 'y') {
+if (responseFive === 'yes' || responseFive === 'y') {
   // console.log('That\'s right ' + userName + '! Too many that I\'ve lost count');
   alert('That\'s right ' + userName + '! Too many that I\'ve lost count');
 } else {
